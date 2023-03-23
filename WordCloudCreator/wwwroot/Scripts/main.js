5e0830aeebd63459852137eb35ece94bc060a3d5
Rick Corsi <cors9056@fredonia.edu>
2023-03-23 00:11:21 -0400

    - Search Functionality - Minor UI Tweaks - 15 titles added

--- WordCloudCreator/wwwroot/Scripts/main.js
+++ WordCloudCreator/wwwroot/Scripts/main.js
@@ -132,22 +132,21 @@ function goSearch() {
 function goFavorites() {
     location.href = "/Favorites";
 }
-function searchAnalyzer() {
-    // Declare variables
-    var input, filter, ul, li, a, i, txtValue;
-    input = document.getElementById('myInput');
-    filter = input.value.toUpperCase();
-    ul = document.getElementById("myUL");
-    li = ul.getElementsByTagName('li');
-    // Loop through all list items, and hide those who don't match the search query
-    for (i = 0; i < li.length; i++) {
-        a = li[i].getElementsByTagName("a")[0];
-        txtValue = a.textContent || a.innerText;
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
-            li[i].style.display = "";
-        } else {
-            li[i].style.display = "none";
+
+function liveSearch() {
+    let cards = document.querySelectorAll('.movieTileMain');
+    var cardsHidden = 0;
+    console.log('Search Run');
+    let search_query = document.getElementById("searchbox").value;
+    for (var i = 0; i < cards.length; i++) {
+        if (cards[i].textContent.toLowerCase()
+            .includes(search_query.toLowerCase())) {
+            cards[i].classList.remove("is-hidden");
+        }
+        else {
+            cards[i].classList.add("is-hidden");
+            cardsHidden++;
         }
     }
-    console.log('Search Parsed');
+    console.log('cardsHidden: ' + cardsHidden);
 }
